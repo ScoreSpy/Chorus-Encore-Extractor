391b2303e1f267ef6352d93931b8107b2f122125
Ahriana <ahrianadev@gmail.com>
2023-03-14 22:36:58 +0000

    extract encrypted archive

---
+++ src/helpers.ts
@@ -0,0 +1,53 @@
+import { access, mkdir, readdir, readFile, writeFile } from 'node:fs/promises'
+import { basename, dirname, extname, join } from 'node:path'
+import { constants as FS_CONSTANTS } from 'node:fs'
+import { createDecipheriv } from 'node:crypto'
+import cryptoConfig from './config/crypto.json'
+import JSZip from 'jszip'
+
+export async function extractEncryptedArchive (input: string, outputDir: string) {
+  const fileContent = await readFile(input)
+  const iv = Buffer.from(fileContent.slice(0, cryptoConfig.IV_LENGTH))
+  const encryptedBuffer = fileContent.slice(16)
+
+  // decrypt the buffer
+  const decipher = createDecipheriv('aes-256-cbc', Buffer.from(cryptoConfig.SECRET_KEY, 'hex'), iv)
+  const decryptedBuffer = Buffer.concat([decipher.update(encryptedBuffer), decipher.final()])
+
+  const zip = await JSZip.loadAsync(decryptedBuffer)
+  await Promise.all(Object.entries(zip.files).map(async ([filename, file]) => {
+    if (!file.dir) {
+      const outputPath = join(outputDir, filename)
+      await mkdir(dirname(outputPath), { recursive: true })
+      await writeFile(outputPath, await file.async('nodebuffer'))
+    }
+  }))
+}
+
+export async function findCEFiles (dirPath: string, files: string[]): Promise<string[]> {
+  const entries = await readdir(dirPath, { withFileTypes: true })
+
+  for (const entry of entries) {
+    const entryPath = join(dirPath, entry.name)
+
+    if (entry.isFile() && extname(entry.name) === '.ce') {
+      files.push(entryPath)
+    } else if (entry.isDirectory()) {
+      await findCEFiles(join(dirPath, entry.name), files)
+    }
+  }
+
+  return files
+}
+
+export function replacePathPart (filePath: string, oldPart: string, newPart: string): string {
+  const dir = dirname(filePath)
+  const base = basename(filePath)
+  const newDir = dir.replace(oldPart, newPart)
+  const newPath = join(newDir, base)
+  return newPath
+}
+
+export function fileExists (path: string): Promise<boolean> {
+  return access(path, FS_CONSTANTS.F_OK).then(() => true).catch(() => false)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log('owo')
+import { mkdir } from 'node:fs/promises'
+import { extractEncryptedArchive, fileExists, findCEFiles, replacePathPart } from './helpers'
 
+const BASE_PATH = 'C:\\Users\\Ahriana\\Documents\\Projects\\Chorus Encore\\Chorus-Encore-Validator\\shadow'
+const SHADOW_PATH = 'C:\\Users\\Ahriana\\Documents\\Projects\\Chorus Encore\\Chorus-Encore-Extractor\\shadow'
+
+async function start () {
+  const ceFiles = await findCEFiles('C:\\Users\\Ahriana\\Documents\\Projects\\Chorus Encore\\Chorus-Encore-Validator\\shadow', [])
+  console.log(ceFiles)
+  console.log(ceFiles.length)
+
+  for (const file of ceFiles) {
+    const outputPath = replacePathPart(file, BASE_PATH, SHADOW_PATH)
+    if (!await fileExists(outputPath)) { await mkdir(outputPath, { recursive: true }) }
+
+    extractEncryptedArchive(file, outputPath)
+  }
+}
+
+start()
